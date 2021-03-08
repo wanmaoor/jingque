a89259b7c9db349450a49749fc6e12812c9012e1
wanmao <wequart@gmail.com>
2021-03-08 17:24:19 +0800

    feat: polishing the pages

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,17 +1,22 @@
 /*
- * @Description: 
- * @version: 
+ * @Description:
+ * @version:
  * @Author: wanmao
  * @LastEditors: wanmao
  */
 import Link from 'next/link';
-export default function FirstPost(){
+import Head from 'next/head';
+import Layout from '../../components/layout';
+export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <Link href="/">
         <a>Go back to main page</a>
       </Link>
       <h1>First Post</h1>
-    </>
+    </Layout>
   );
 }
